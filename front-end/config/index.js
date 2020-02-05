96e0f5eead74a8b9cb8b5cc33d33473ae4d8fa64
yuanweimin <ywm00@qq.com>
2020-02-05 18:40:50 +0800

    add jupyter demo

--- front-end/config/index.js
+++ front-end/config/index.js
@@ -2,10 +2,10 @@ export default {
     canvas: {
         width: 280, // 画布宽度
         height: 280, // 画布高度
-        bgColor: '#fff', // 白色背景
+        bgColor: '#000', // 背景颜色
         scale: 1, // 缩放比例
         lineWidth: 5, // 画笔粗细
-        lineStyle: '#000' // 画笔颜色
+        lineStyle: '#fff' // 画笔颜色
     },
     paint: {
         keyCode: 32, // 空格键开始绘画
